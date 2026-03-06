4c8e2a83a2899c7089807a57d1db75fceaec07f0
chandrakala-kombathula <kombathulachandrakala05@gmail.com>
2026-03-06 19:42:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import PyPDF2
 import matplotlib.pyplot as plt
+import google.generativeai as genai
+import streamlit as st
+
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+model = genai.GenerativeModel("gemini-pro")
+
 
 # Load CSS
 def load_css():
